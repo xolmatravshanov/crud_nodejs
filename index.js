93e80f16892f3dc380d20fb0d7ede1e8914ad171
Xolmat <xolmatrw@gmail.com>
2021-09-14 14:43:33 +0500

    index.js has changed

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // parse requests of content-type - application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({extended: true}))
 // parse requests of content-type - application/json
 app.use(bodyParser.json())
 
@@ -23,9 +23,9 @@ app.get('/', (req, res) => {
 // Require employee routes
 const employeeRoutes = require('./src/routes/employee.routes')
 
-
+const BASE_V1_ENDPOINT = "/api/v1/";
 // using as middleware
-app.use('/api/v1/employees', employeeRoutes)
+app.use(`${BASE_V1_ENDPOINT}employees`, employeeRoutes)
 
 // listen for requests
 app.listen(port, () => {
