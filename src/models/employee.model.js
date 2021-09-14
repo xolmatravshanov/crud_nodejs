3e59a5cf08143c16afa1ec7eedc3f9357a03e062
Xolmat <xolmatrw@gmail.com>
2021-09-14 14:17:59 +0500

    Employee model has changed

--- src/models/employee.model.js
+++ src/models/employee.model.js
@@ -3,26 +3,25 @@
 var dbConn = require('../config/db.config');
 
 //Employee object create
-var Employee = function(employee){
-    this.first_name     = employee.first_name;
-    this.last_name      = employee.last_name;
-    this.email          = employee.email;
-    this.phone          = employee.phone;
-    this.organization   = employee.organization;
-    this.designation    = employee.designation;
-    this.salary         = employee.salary;
-    this.status         = employee.status ? employee.status : 1;
-    this.created_at     = new Date();
-    this.updated_at     = new Date();
+var Employee = function (employee) {
+    this.first_name = employee.first_name;
+    this.last_name = employee.last_name;
+    this.email = employee.email;
+    this.phone = employee.phone;
+    this.organization = employee.organization;
+    this.designation = employee.designation;
+    this.salary = employee.salary;
+    this.status = employee.status ? employee.status : 1;
+    this.created_at = new Date();
+    this.updated_at = new Date();
 };
 
 Employee.create = function (newEmp, result) {
     dbConn.query("INSERT INTO regions set ?", newEmp, function (err, res) {
-        if(err) {
+        if (err) {
             console.log("error: ", err);
             result(err, null);
-        }
-        else{
+        } else {
             console.log(res.insertId);
             result(null, res.insertId);
         }
@@ -30,12 +29,11 @@ Employee.create = function (newEmp, result) {
 };
 
 Employee.findById = function (id, result) {
-    dbConn.query("Select * from regions where id = ? ", id, function (err, res) {
-        if(err) {
+    dbConn.query("select * from regions where id = ? ", id, function (err, res) {
+        if (err) {
             console.log("error: ", err);
             result(err, null);
-        }
-        else{
+        } else {
             result(null, res);
         }
     });
@@ -43,12 +41,10 @@ Employee.findById = function (id, result) {
 
 Employee.findAll = function (result) {
     dbConn.query("Select * from regions", function (err, res) {
-        if(err) {
+        if (err) {
             console.log("error: ", err);
-            result(null, 'result');
             result(null, err);
-        }
-        else{
+        } else {
             console.log('regions : ', res);
             result(null, res);
         }
@@ -56,26 +52,26 @@ Employee.findAll = function (result) {
 };
 
 
-Employee.update = function(id, employee, result){
-    dbConn.query("UPDATE regions SET first_name=?,last_name=?,email=?,phone=?,organization=?,designation=?,salary=? WHERE id = ?", [employee.first_name,employee.last_name,employee.email,employee.phone,employee.organization,employee.designation,employee.salary, id], function (err, res) {
-        if(err) {
-            console.log("error: ", err);
-            result(null, err);
-        }else{
-            result(null, res);
-        }
-    });
+Employee.update = function (id, employee, result) {
+    dbConn.query("UPDATE regions SET first_name=?,last_name=?,email=?,phone=?,organization=?,designation=?,salary=? WHERE id = ?", [employee.first_name, employee.last_name, employee.email, employee.phone, employee.organization, employee.designation, employee.salary, id],
+        function (err, res) {
+            if (err) {
+                console.log("error: ", err);
+                result(null, err);
+            } else {
+                result(null, res);
+            }
+        });
 };
 
-Employee.delete = function(id, result){
+Employee.delete = function (id, result) {
     dbConn.query("DELETE FROM regions WHERE id = ?", [id], function (err, res) {
-        if(err) {
+        if (err) {
             console.log("error: ", err);
             result(null, err);
-        }
-        else{
+        } else {
             result(null, res);
         }
     });
 };
-module.exports= Employee;
+module.exports = Employee;
