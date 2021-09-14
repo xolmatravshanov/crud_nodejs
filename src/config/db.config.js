de27bec1e41ef5589dd982ef29840da713cf832c
Xolmat <xolmatrw@gmail.com>
2021-09-14 14:07:32 +0500

    Dot was added to the project

---
+++ src/config/db.config.js
@@ -0,0 +1,24 @@
+'use strict';
+
+
+const mysql = require('mysql');
+const donenv = require('dotenv');
+donenv.config();
+//local mysql db connection
+
+const dbConn = mysql.createConnection({
+    host     : process.env.DB_HOST,
+    user     : process.env.DB_USER,
+    password : process.env.DB_PASSWORD,
+    database : process.env.DB_NAME
+});
+
+
+
+dbConn.connect(function(err) {
+    if (err) throw err;
+    console.log("Database Connected!");
+});
+
+
+module.exports = dbConn;
