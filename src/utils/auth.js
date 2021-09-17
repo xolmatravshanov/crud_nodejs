eb1aca60e4c95b93ef9f6d35cba6e202292eafb2
Xolmat <xolmatrw@gmail.com>
2021-09-17 10:38:22 +0500

    fkjsk

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,13 +1,24 @@
-const auth = {
+var jwt = require('jsonwebtoken');
+
+const Jwt = {
 
     /**
      *
      */
     secretKey: 'secretKey',
+
     /**
      *
      */
     publicKey: 'publicKey',
+
+    /**
+     *
+     * hourly expire example '1h'
+     * days expire example '7d'
+     * milli seconds expire example '100'
+     */
+    expiresIn: 60 * 60,
 
     /**
      * @param req
@@ -22,8 +33,31 @@ const auth = {
         } else {
             res.sendStatus(403);
         }
+    },
+
+    verifyTokenSecond: (req, res, next) => {
+        const token =
+            req.body.token || req.query.token || req.headers["x-access-token"];
+
+        if (!token) {
+            return res.status(403).send("A token is required for authentication");
+        }
+
+        try {
+            req.user = jwt.verify(token, config.TOKEN_KEY);
+        } catch (err) {
+            return res.status(401).send("Invalid Token");
+        }
+
+        return next();
+    },
+
+
+    sign: (signObject) => {
+        jwt.sign(signObject, this.secretKey, {expiresIn: this.expiresIn});
     }
+
 
 };
 
-module.exports = auth;
+module.exports = Jwt;
